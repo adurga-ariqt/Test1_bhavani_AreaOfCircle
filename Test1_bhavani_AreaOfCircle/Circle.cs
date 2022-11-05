9cc5a42c6b62ae00a0d0606af1259b661caf037b
adurga-ariqt <adurga@ariqt.com>
2022-11-05 16:14:09 +0530

    Add project files.

---
+++ Test1_bhavani_AreaOfCircle/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_bhavani_AreaOfCircle
+{
+    public class Circle
+    {
+        double pi = 3.14;
+        double areaOfCircle;
+        double perimeter;
+        public int Radius { get; set; }
+
+        public void  GerCircleAndPerimeter()
+        { areaOfCircle = pi * Radius * Radius;
+            Console.WriteLine(" Area of the Circle:" + areaOfCircle);
+            perimeter = 2 * pi * Radius;
+            Console.WriteLine(" perimeter of the circle :" + perimeter);
+        }
+    }
+}
---
+++ Test1_bhavani_AreaOfCircle/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test1_bhavani_AreaOfCircle;
+
+Console.WriteLine("Hello, World!");
+Circle obj = new Circle();
+Console.WriteLine("enter radius:");
+obj.Radius = Convert.ToInt32(Console.ReadLine());
+obj.GerCircleAndPerimeter();
+
